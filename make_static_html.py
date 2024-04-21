60f48ee287fe810cf78b25c62285fed947719342
Matthew Reda <m.reda94@gmail.com>
2024-04-20 21:56:16 -0400

    updated index.md

--- make_static_html.py
+++ make_static_html.py
@@ -10,5 +10,5 @@ markdown_file_paths = {file.name: file for file in Path("app/static").glob("*.md
 
 
 string = "\n\n".join([markdown_file_paths[file].read_text() for file in order if file in markdown_file_paths.keys()])
-with open("static_html.md", "w") as f:
+with open("index.md", "w") as f:
     f.write(string)
