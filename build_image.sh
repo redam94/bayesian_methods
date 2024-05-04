74f0ad1235f3dd9a06b7195c28834fd19f5126aa
Matthew Reda <m.reda94@gmail.com>
2024-05-04 18:41:52 -0400

    cleaned up directory

--- build_image.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-docker build -t redam94/regression:$(git rev-parse --abbrev-ref HEAD| sed 's/[^a-zA-Z0-9]//g') .
-docker push redam94/regression:$(git rev-parse --abbrev-ref HEAD| sed 's/[^a-zA-Z0-9]//g')
-
-
